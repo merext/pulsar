e26f12b7ef314fd37f1355ec2e76aef6af9eb7f6
Maksim Korenev <unzoom@gmail.com>
2025-05-10 18:05:43 +0800

    up

---
+++ bots/binance/src/main.rs
@@ -0,0 +1,14 @@
+use binance_exchange::client::BinanceClient;
+use env_logger::Builder;
+use log::LevelFilter;
+
+#[tokio::main]
+async fn main() {
+    Builder::from_default_env()
+        .filter_level(LevelFilter::Debug)
+        .init();
+
+    if let Err(e) = BinanceClient::subscribe_klines("BTCUSDT").await {
+        eprintln!("WebSocket subscription failed: {}", e);
+    }
+}
---
+++ exchanges/binance/src/client.rs
@@ -0,0 +1,43 @@
+use binance_spot_connector_rust::{
+    market::klines::KlineInterval,
+    market_stream::kline::KlineStream,
+    tokio_tungstenite::BinanceWebSocketClient,
+};
+use futures_util::StreamExt;
+use log::info;
+
+pub struct BinanceClient;
+
+impl BinanceClient {
+    pub async fn subscribe_klines(symbol: &str) -> Result<(), Box<dyn std::error::Error>> {
+        // Connect to WebSocket
+        let (mut conn, _) = BinanceWebSocketClient::connect_async_default().await?;
+
+        // Subscribe to 1-minute Kline stream for the given symbol
+        conn.subscribe(vec![
+            &KlineStream::new(symbol, KlineInterval::Minutes1).into()
+        ]).await;
+
+        info!("Subscribed to 1m Kline stream for {}", symbol);
+
+        // Read messages indefinitely
+        while let Some(message) = conn.as_mut().next().await {
+            match message {
+                Ok(msg) => {
+                    let binary_data = msg.into_data();
+                    let data = std::str::from_utf8(&binary_data)?;
+                    info!("Received: {}", data);
+                }
+                Err(e) => {
+                    eprintln!("Error receiving message: {:?}", e);
+                    break;
+                }
+            }
+        }
+
+        // Cleanly close connection if the loop exits
+        conn.close().await?;
+
+        Ok(())
+    }
+}
